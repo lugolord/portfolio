776087ff9d319f5d491eba09b8ac115372c8facf
Luis Gonzalez <lugo50635@gmail.comm>
2021-12-06 18:21:32 -0300

    buenas practicas

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let body = document.querySelector('body');
 
 window.onload = () => {
-    console.log('se cargo todo');
+
     body.animate({opacity: 1}, 2000);
+    
     setTimeout(() => {body.style.opacity = 1}, 2000);
 }
