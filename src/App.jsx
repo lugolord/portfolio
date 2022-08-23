b4a9a363ca8225303aac6eacd6767ad8d45b7798
luis <lugo50635@gmail.comm>
2022-08-23 17:24:42 -0300

    version 0.1.2, minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from './components/Header';
 import AboutSwiper from './components/AboutSwiper';
 import Portfolio from './components/Portfolio';
 import Studies from './components/Studies';
-import ToTopBtn from './components/toTopBtn';
+import ToTopBtn from './components/ToTopBtn';
 import Footer from './components/Footer';
 
 function App() {
