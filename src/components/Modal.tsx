b3dca44abbd6a11308d285d98bff89a4df4aa582
Luis Gonzalez <lugo50635@gmail.com>
2026-01-18 16:22:20 -0300

    fix: acomode el tamaño del modal de los trabajos en celulares

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,7 +10,7 @@ function Modal ({ description, id }: { description: string, id: number }) {
         detalles
       </button>
       <dialog id={`modal-${id}`} className="modal">
-        <div className="modal-box w-96 max-w-5xl">
+        <div className="modal-box w-72 sm:w-96 max-w-5xl">
           <h3 className="font-bold text-lg mb-3">Detalles del puesto</h3>
           <div className="space-y-4 text-sm dark:text-neutral-300">
             {description.split('\n').map((p, i) => (
